5a3fe2077a6e1d0f54417ebea3ce2529c306cd2d
KKudas <143763586+KKudas@users.noreply.github.com>
2023-09-08 13:44:08 +0800

    Update TestMain.java

--- 2103/EmployeeProject/src/employeeproject/TestMain.java
+++ 2103/EmployeeProject/src/employeeproject/TestMain.java
@@ -15,8 +15,13 @@ public class TestMain {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) { 
+    public static void main(String[] args) {
+        HourlyEmployee employee1 = new HourlyEmployee(12, "asdf", "10/02/2003", "10/02/2020", 35, 76.25F);
+        PieceWorkerEmployee employee2 = new PieceWorkerEmployee(14, "fdsa", "1/1/11", "2/2/22", 200, 76.25F);
+        CommissionEmployee employee3 = new CommissionEmployee(16, "vczv", "2/1/21", "3/4/5", 12000);
+        BasePlusCommisionEmployee employee4 = new BasePlusCommisionEmployee(16, "vczv", "2/2/11", "6/4/15", 5000);
 
+        double salaryOfEmployee1 = employee1.computeSalary();
+        System.out.println("Employee 1 Salary is: " + salaryOfEmployee1);
     }
-
 }
