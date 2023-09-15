09d260e5013b9a59c587543894d282e98df8b6b7
KKudas <143763586+KKudas@users.noreply.github.com>
2023-09-15 09:35:24 +0800

    Update TestMain.java
    Added:
    - Print all information
    - Replaced All date of birth and hired
    Bug:
    - Can't run

--- 2103/EmployeeProject/src/employeeproject/TestMain.java
+++ 2103/EmployeeProject/src/employeeproject/TestMain.java
@@ -5,6 +5,9 @@
  */
 package employeeproject;
 import employee.version1.*;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 
 /**
  *
@@ -16,21 +19,29 @@ public class TestMain {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        HourlyEmployee employee1 = new HourlyEmployee(12, "asdf", "10/02/2003", "10/02/2020", 35, 76.25F);
-        PieceWorkerEmployee employee2 = new PieceWorkerEmployee(14, "fdsa", "1/1/11", "2/2/22", 200, 76.25F);
-        CommissionEmployee employee3 = new CommissionEmployee(16, "vczv", "2/1/21", "3/4/5", 12000);
-        BasePlusCommisionEmployee employee4 = new BasePlusCommisionEmployee(16, "vczv", "2/2/11", "6/4/15", 5000, 12000);
-
+        //No INPUT STRING
+        HourlyEmployee employee1 = new HourlyEmployee(12, "Ralph Mandigma", "02-10-2003", "10-02-2020", 35, 76.25F);
+        PieceWorkerEmployee employee2 = new PieceWorkerEmployee(14, "Miguel Mandigma", "01-10-2011", "02-02-2022", 200, 76.25F);
+        CommissionEmployee employee3 = new CommissionEmployee(16, "Rapahel Dela Rosa", "02-10-2021", "03-05-2023", 12000);
+        BasePlusCommisionEmployee employee4 = new BasePlusCommisionEmployee(16, "Michael Dela Rosa", "02-02-2011", "06-04-2015", 5000, 12000);  
+        
         double salaryOfEmployee1 = employee1.computeSalary();
-        System.out.println("Employee 1 Salary is: " + salaryOfEmployee1);
-
+        System.out.println(employee1.getEmpName() + " Salary is: " + salaryOfEmployee1);
+        employee1.displayInfo(salaryOfEmployee1);
+        System.out.printf("\n");
+        
         double salaryOfEmployee2 = employee2.computeSalary();
-        System.out.println("Employee 2 Salary is: " + salaryOfEmployee2);
+        System.out.println(employee2.getEmpName() + " Salary is: " + salaryOfEmployee2);
+        employee2.displayInfo(salaryOfEmployee2);
+        System.out.printf("\n");
 
         double salaryOfEmployee3 = employee3.computeSalary();
-        System.out.println("Employee 3 Salary is: " + salaryOfEmployee3);
+        System.out.println(employee3.getEmpName() + " Salary is: " + salaryOfEmployee3);
+        employee3.displayInfo(salaryOfEmployee3);
+        System.out.printf("\n");
 
         double salaryOfEmployee4 = employee4.computeSalary();
-        System.out.println("EMployee 4 Salary is: " + salaryOfEmployee4);
+        System.out.println(employee4.getEmpName() + " Salary is: " + salaryOfEmployee4);
+        employee4.displayInfo(salaryOfEmployee4);
     }
 }
